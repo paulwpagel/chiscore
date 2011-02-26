5fb04746773b35dc1f89d499c41ccd892b2ab0c8
Paul Pagel <paul@8thlight.com>
2011-02-26 12:03:07 -0600

    totals for routes

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe Team do
+  
+  it "has checkins" do
+    @team = Team.create!(:name => "team")
+    checkin1 = TeamCheckin.create!(:team => @team)
+    checkin2 = TeamCheckin.create!(:team => @team)
+    @team.team_checkins.should include(checkin1) 
+    @team.team_checkins.should include(checkin2) 
+  end
 end
