2362ed4010ddeff92d2fd9d341abeb6988887e15
Brian Pratt <brian@8thlight.com>
2012-02-22 14:47:24 -0600

    unneeded migrations -> seeya

--- db/migrate/20120221192340_rename_vote_to_count.rb
+++
@@ -1,9 +0,0 @@
-class RenameVoteToCount < ActiveRecord::Migration
-  def self.up
-    rename_column(:votes, :vote, :count)
-  end
-
-  def self.down
-    rename_column(:votes, :count, :vote)
-  end
-end
--- db/migrate/20120221192949_add_default_value_to_vote_count.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultValueToVoteCount < ActiveRecord::Migration
-  def self.up
-    change_column(:votes, :count, :integer, :default => 0)
-  end
-
-  def self.down
-    change_column(:votes, :count, :integer)
-  end
-end
